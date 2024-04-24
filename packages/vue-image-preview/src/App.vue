b56957646baacf737b588ba402cab50d7094e429
mouday <pengshiyuyx@gmail.com>
2024-04-24 03:32:37 +0000

    Deploying to gh-pages from @ mouday/vue-demo@daaaa8bd8bd8d179da13b98e314b7c3cc89f5b96 🚀

---
+++ packages/vue-image-preview/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="">
+    <ImagePreviewDemo></ImagePreviewDemo>
+  </div>
+</template>
+
+<script>
+// created at 2022-12-26
+import ImagePreview from "@/components/image-preview/index.js";
+import ImagePreviewDemo from "./components/ImagePreview.vue";
+
+export default {
+  name: "App",
+
+  mixins: [ImagePreview],
+
+  props: {},
+
+  components: {
+    ImagePreviewDemo,
+  },
+
+  data() {
+    return {};
+  },
+
+  computed: {},
+
+  methods: {
+    async getData() {},
+  },
+
+  created() {
+    this.getData();
+  },
+};
+</script>
+
+<style lang="less"></style>
+
+<style lang="less" scoped></style>
