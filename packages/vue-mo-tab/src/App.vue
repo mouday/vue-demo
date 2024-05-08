a73c784f2b7ce6a2adc870fe8102eb601b0f6627
mouday <pengshiyuyx@gmail.com>
2024-05-08 07:13:33 +0000

    Deploying to gh-pages from @ mouday/vue-demo@7285e59d9b6c40c74c38a9c4099337d3eef097fa 🚀

---
+++ packages/vue-mo-tab/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="app">
+    <MoTab
+      :options="options"
+      v-model="value"
+      @on-change="handleValueChange"
+    ></MoTab>
+
+    <div class="content">
+      <p>选定真实值: {{ value }}</p>
+      <p>选定显示值: {{ currentLabel }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// created at 2023-03-31
+import MoTab from "./components/MoTab/MoTab.vue";
+
+export default {
+  name: "App",
+
+  props: {},
+
+  components: {
+    MoTab,
+  },
+
+  data() {
+    return {
+      // 选定值
+      value: "1",
+
+      // 选项
+      options: [
+        {
+          value: "1",
+          label: "蔬菜",
+        },
+        {
+          value: "2",
+          label: "肉类",
+        },
+        {
+          value: "3",
+          label: "水果",
+        },
+      ],
+    };
+  },
+
+  computed: {
+    currentLabel() {
+      return this.options.find((item) => item.value === this.value)?.label;
+    },
+  },
+
+  methods: {
+    handleValueChange(value) {
+      console.log("value change: ", value);
+    },
+  },
+
+  created() {},
+};
+</script>
+
+<style lang="less">
+.app {
+  width: 200px;
+  margin: 0 auto;
+  border: 1px solid #ccc;
+  padding: 20px;
+}
+
+.content {
+  margin-top: 20px;
+  border-top: 1px solid #ccc;
+}
+</style>
---
+++ packages/vue-mo-tab/src/components/MoTab/MoTab.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="mo-tab">
+    <div
+      v-for="item in options"
+      class="mo-tab__item"
+      :class="{
+        'mo-tab__item--active': item.value == currentValue,
+      }"
+      @click="handleChange(item.value)"
+    >
+      {{ item.label }}
+    </div>
+  </div>
+</template>
+
+<script>
+// created at 2024-05-08
+export default {
+  name: "MoTab",
+
+  props: {
+    // 选项
+    // eg: [{label: '显示值', value: '真实值'}]
+    options: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+
+    // 选定值
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+
+  computed: {
+    currentValue: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.$emit("input", val);
+      },
+    },
+  },
+
+  methods: {
+    handleChange(value) {
+      this.currentValue = value;
+      this.$emit("on-change", value);
+    },
+  },
+};
+</script>
+
+<style lang="less" src="./MoTab.less"></style>
